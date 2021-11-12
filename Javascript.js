5e70a85a2f960880e8c7ce02695577184c204a73
Abdul Rahman <abdr60699@gmail.com>
2021-11-12 18:52:30 +0530

    JS Practice file-1

---
+++ Javascript.js
@@ -0,0 +1,50 @@
+﻿//maths
+var num = 1;
+var num2 = 3;
+console.log(num * num2);
+
+
+//array
+var animal = ["tiger", "lion", "dog", 96];
+console.log(animal[2]);
+//for
+
+for (var i = 1; i <= 2; i++) {
+    console.log(i);
+}
+var india = "dubai";
+console.log(india);
+//while
+var i = 1;
+while (i <= 2) {
+    console.log(i);
+    i++
+}
+//forEach
+var numberList =[1,2,3,4];
+numberList.forEach(function(num)
+{
+    console.log(num);
+}
+)
+ var a = 2;
+ var c= "2";
+ var d =2;
+
+ if (c === a){
+     console.log("True");
+ }
+ else{
+     console.log("false");
+ }
+
+ var color = "black";
+ switch (color){
+case "red" : 
+case "blue": 
+console.log("i like red");
+console.log("i hate blue");
+break;
+ default : console.log("choose the color")
+
+ }
